7aa3a36bbad012ec10de7832a24dc9528ce39cf0
pixinga <106994908+pixinga@users.noreply.github.com>
2022-06-07 13:05:56 -0300

    Update agendaShows.tsx

--- src/__mock__/agendaShows.tsx
+++ src/__mock__/agendaShows.tsx
@@ -3,13 +3,13 @@ export const agendaShows = [
         dia: '19',
         mes: 'Junho',
         local: 'Heavy Beer - Rua Ceara',
-        hora: '23:00'
+        hora: '17:00'
     },
     {
         dia: '16',
         mes: 'Julho',
         local: 'Audio Rebel',
-        hora: '22:00'
+        hora: '18:00'
     },
     {
         dia: '',
