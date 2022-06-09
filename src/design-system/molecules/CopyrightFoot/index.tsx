0f6d10b4be6f323332256f3cca7964244e113f2f
Ricardo Côrtes <giralander@hotmail.com>
2022-06-09 19:16:38 +0200

    Added all images modifiable

--- src/design-system/molecules/CopyrightFoot/index.tsx
+++ src/design-system/molecules/CopyrightFoot/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Grid, Link, useMediaQuery } from '@mui/material'
+import { Box, Divider, Grid, Link, Stack, useMediaQuery } from '@mui/material'
 import { FC } from 'react'
 import { Copyright } from '../../atoms/Copyright'
 
@@ -21,9 +21,11 @@ const CopyrightFoot: FC<CopyrightFootProps> = ({ pageWidth }) => {
                 sx={{ padding: '.5rem 3rem' }}
             >
                 <Grid item xs={12} md={6} alignSelf='center'>
-                    <Typography size='small'>Desenvolvedor</Typography>
-                    <Divider orientation="vertical" style={{ borderColor: "rgba(255, 255, 255, .2)" }} flexItem />
-                    <a href="http://cortesdev.de" target="_blank">{' '} CortesDev.de</a>
+                    <Stack direction="row">
+                        <Typography size='small'>Desenvolvedor</Typography>
+                        <Divider orientation="vertical" style={{ margin: '0 .5rem', borderColor: "rgba(255, 255, 255, .2)" }} flexItem />
+                        <Typography size='small'><a href="http://cortesdev.de" target="_blank">{' '} CortesDev.de - germany</a></Typography>
+                    </Stack>
                 </Grid>
 
                 <Grid item xs={12} md={6}>
