436143efb793703cdb0c29240367185c8dde2d5b
Ricardo Côrtes <giralander@hotmail.com>
2022-06-09 12:50:03 +0200

    Add spotify

--- src/design-system/organisms/Footer/index.tsx
+++ src/design-system/organisms/Footer/index.tsx
@@ -34,7 +34,7 @@ const Footer: FC<FooterProps> = ({ pageWidth }) => {
     `
 
     const linkData = [
-        { src: '/images/SVG/twitter.svg', alt: 'facebook', url: 'https://www.twitter.com/doidonpixote' },
+        { src: '/images/SVG/twitter.svg', alt: 'twitter', url: 'https://www.twitter.com/doidonpixote' },
         { src: '/images/SVG/facebook.svg', alt: 'facebook', url: 'https://www.facebook.com/doidonpixote' },
         { src: '/images/SVG/insta.svg', alt: 'insta', url: 'https://www.instagram.com/doidonpix/' },
         { src: '/images/SVG/spotify.svg', alt: 'spotify', url: 'https://open.spotify.com/artist/1H3kmZKczSXJ2jum4zrqR3' },
